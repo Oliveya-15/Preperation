b797249a55d8c4fb82f918604a8a1849eafd1b1e
Oliveya-15 <162950958+Oliveya-15@users.noreply.github.com>
2026-04-26 16:53:24 +0530

    Day - 10

--- DSA/L10.py
+++ DSA/L10.py
@@ -4,7 +4,7 @@ Given a string s consisting of words and spaces, return the length of the last w
 A word is a maximal substring consisting of non-space characters only.
 """
 
-#CODDE HERE :
+#CODE HERE :
 
 class Solution(object):
     def lengthOfLastWord(self, s):
