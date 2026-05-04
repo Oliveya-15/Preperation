b7f040aaf845cb75658d63265377243dbe9677ee
Oliveya Choudhury <choudhuryoliveya15@gmail.com>
2026-05-04 18:13:31 +0530

    Day - 18

---
+++ DSA/L18.py
@@ -0,0 +1,48 @@
+# Same Tree
+
+"""
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+Constraints:
+
+The number of nodes in both trees is in the range [0, 100].
+-104 <= Node.val <= 104
+"""
+
+# CODE HERE
+class Solution(object):
+    def isSameTree(self, p, q):
+        # If both are None
+        if not p and not q:
+            return True
+        
+        # If one is None
+        if not p or not q:
+            return False
+        
+        # If values differ
+        if p.val != q.val:
+            return False
+        
+        # Check left and right subtree
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+"""
+Example 1:
+
+
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
+Example 2:
+
+
+Input: p = [1,2], q = [1,null,2]
+Output: false
+Example 3:
+
+
+Input: p = [1,2,1], q = [1,1,2]
+Output: false
+ 
+"""
