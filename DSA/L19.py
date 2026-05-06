30b1621a9d33c91114b52a672e830583e1c8fb7f
Oliveya Choudhury <choudhuryoliveya15@gmail.com>
2026-05-06 13:04:31 +0530

    Day - 19

---
+++ DSA/L19.py
@@ -0,0 +1,35 @@
+# Maximum Depth of Binary Tree
+
+"""
+Given the root of a binary tree, return its maximum depth.
+A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 104].
+-100 <= Node.val <= 100
+"""
+
+# CODE HERE:
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        if not root:
+            return 0
+        
+        left_depth = self.maxDepth(root.left)
+        right_depth = self.maxDepth(root.right)
+        
+        return 1 + max(left_depth, right_depth)
+
+"""
+Input: root = [3,9,20,null,null,15,7]
+Output: 3
+Example 2:
+
+Input: root = [1,null,2]
+Output: 2
+"""
